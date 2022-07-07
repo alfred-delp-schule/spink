0fde2f636d47a6b3291fbd6fe1b80e41cc4c0ab4
jk <72466440+jk10274@users.noreply.github.com>
2022-07-07 21:58:55 +0200

    Add working login page

--- kunde/login/index.php
+++ kunde/login/index.php
@@ -47,11 +47,49 @@
 <!DOCTYPE html> 
 <html> 
 <head>
-  <title>Anmeldung</title>    
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>SPINK - Log in</title>
+    <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="../../img/favicon_frame3.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../img/favicon_frame1.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../img/favicon_frame2.png">
+    <link rel="icon" type="image/png" sizes="180x180" href="../../img/favicon_frame3.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../../img/favicon_frame4.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="../../img/favicon_frame5.png">
+    <link rel="stylesheet" href="../../styles/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../styles/css/ClashDisplay-Variable.css">
+    <link rel="stylesheet" href="../../styles/css/Navbar-With-Button.css">
+    <link rel="stylesheet" href="../../styles/css/styles.css">   
 </head> 
-<body>
+<body style="background: rgb(255,246,232); margin-top: 3em;">
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<section>
+      <a href="https://spink-trade.de"><img class="user-select-none mx-auto d-block" src="../../img/logo_spink.png" width="120px"></a>
+        <h1 class="text-center pb-5">Log in</h1>
+        <div class="row mb-3">
+          <div class="col-lg-4 themed-grid-col"></div>
+          <div class="col-lg-4 themed-grid-col">
+            <div style="background-color: #fdfdfd;" class="rounded-3">
+              <form class="p-4" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email Adresse</label>
+                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Passwort</label>
+                    <input type="password" class="form-control" name="passwort" id="exampleInputPassword1">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Log in</button>
+                  <div class="form-text mt-4">Hast du noch keinen Account? <a href="/kunde/signup">Sign Up</a></div>
+                </form>
+          </div>
+          <div class="col-lg-4 themed-grid-col"></div>
+        </div>
+        
+        </div>
+    </section>
+
+<!-- <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 E-Mail:<br>
 <input type="email" size="40" maxlength="250" name="email"><br><br>
  
@@ -66,13 +104,15 @@ Dein Passwort:<br>
 Zum <a href="../signup"> Registrieren </a>
 Zur <a href="../../"> Startseite </a>
 
-</p>
+</p> -->
 
 
 <?php
     }
 
 ?>
+
+<?php include('../../view/footer.php'); ?>
 
 </body>
 </html>
--- view/footer.php
+++ view/footer.php
@@ -1,10 +1,10 @@
 <footer class="text-center">
         <div class="container text-muted py-4 py-lg-5">
             <ul class="list-inline" style="font-family: ClashDisplay-Variable;">
-                <li class="list-inline-item" style="margin-right: 24px;"><a class="link-secondary" href="public">Marktplatz</a></li>
+                <li class="list-inline-item" style="margin-right: 24px;"><a class="link-secondary" href="/public">Marktplatz</a></li>
                 <li class="list-inline-item" style="margin-right: 24px;"><a class="link-secondary" href="mailto:info@spink-trade.de">Email uns</a></li>
-                <li class="list-inline-item" style="margin-right: 24px;"><a class="link-secondary" href="impressum">Impressum</a></li>
-                <li class="list-inline-item" style="margin-right: 8px;"><a class="link-secondary" href="datenschutzerklärung">Datenschutzerklärung</a></li>
+                <li class="list-inline-item" style="margin-right: 24px;"><a class="link-secondary" href="/impressum">Impressum</a></li>
+                <li class="list-inline-item" style="margin-right: 8px;"><a class="link-secondary" href="/datenschutz">Datenschutzerklärung</a></li>
             </ul>
             <p class="mb-0" style="font-family: ClashDisplay-Variable;">Copyright © <span id="year_span"></span> SPINK</p>
         </div>
